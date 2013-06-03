c6204dca74075448f397a7c980887c0a470b915c
ilya.shaisultanov <ilya.shaisultanov@gmail.com>
2013-06-02 21:35:20 -0400

    Stop interference between instances.
    Use two sockets per instance: server for listening
    and client for sending.
    
    Change-Id: Ifdc4454f5f673aae92c0a336be11c4e5836492df

--- Makefile
+++ Makefile
@@ -1,10 +1,10 @@
-TESTS = test/*
-REPORTER = spec
+TESTS=test/*
+REPORTER=spec
 
 test:
-	@COVERAGE= mocha \
+	@COVERAGE= NODE_ENV=test mocha \
 		--reporter $(REPORTER) \
-		--timeout 0 \
+		--timeout 28s \
 		--growl \
 		--bail \
 		$(TESTS)
@@ -33,28 +33,29 @@ docs:
 	@yuidoc
 
 clean:
-	@rm -rf lib-cov build
+	@rm -rf lib-cov build reports .sonar
 
 .PHONY: test test-watch
 
 xunit:
 	@# check if reports folder exists, if not create it
 	@test -d reports || mkdir reports
-	NODE_ENV=test XUNIT_FILE="reports/TESTS-xunit.xml" mocha -R xunit-file $(TESTS)
+	@NODE_ENV=test XUNIT_FILE="reports/TESTS-xunit.xml" mocha -R xunit-file -t 20s $(TESTS)
 	
 coverage:
 	@# check if reports folder exists, if not create it
 	@test -d reports || mkdir reports
-	istanbul instrument --output lib-cov lib
+	@istanbul instrument --output lib-cov lib
 	@# move original src code and replace it by the instrumented one
-	mv lib lib-orig && mv lib-cov lib
+	@mv lib lib-orig && mv lib-cov lib
 	@# tell istanbul to only generate the lcov file
-	ISTANBUL_REPORTERS=lcovonly mocha -R mocha-istanbul $(TESTS)
+	ISTANBUL_REPORTERS=lcovonly NODE_ENV=test mocha -R mocha-istanbul -t 20s $(TESTS)
 	@# place the lcov report in the report folder, remove instrumented code
 	@# and reput src at its place
-	mv lcov.info reports/coverage.lcov
-	rm -rf lib
-	mv lib-orig lib
+	@mv lcov.info reports/coverage.lcov
+	@rm -rf lib
+	@mv lib-orig lib
 
 sonar:
 	sonar-runner
+	@rm -rf .sonar
--- test/discovery.test.js
+++ test/discovery.test.js
@@ -3,22 +3,53 @@ var Disco = require('../')
 
 
 
-describe('Defaults', function () {
+describe('Default announcements', function () {
   describe('Two instances', function () {
-    it('see each other', function (done) {
-      var server1 = Disco()
-        , server2 = Disco()
-        , messageCount = 0;
+    it('can each other', function (done) {
+      var server1 = new Disco
+        , server2 = new Disco
+        , messageCount = 0
       
-      [server1, server2].forEach(function(server){
+      assert.notEqual(server1.getId(), server2.getId())
+      
+      ;[server1, server2].forEach(function(server){
         server.on('message', function(msg){
           assert(/default discovery/i.test(msg))
-          if (++messageCount == 2) done()
+          if (++messageCount == 2) {
+            server1.stop(function(){
+              server2.stop(done)
+            })
+          }
         })
       })
       
-      server1.start();
-      server2.start();
+      server1.start(function(){
+        server2.start()
+      })
+    })
+    
+    it('do not interfere with each other', function (done) {
+      var opts = {multicastLoopback: true, port: Disco.getRandomPort()}
+        , server1 = Disco(opts)
+        , server2 = Disco(opts)
+        , messageCount = 0
+
+      assert.notEqual(server1.getId(), server2.getId())
+
+      ;[server1, server2].forEach(function(server){
+        server.on('message', function(msg){
+          assert(/default discovery/i.test(msg))
+          if (++messageCount == 2) {
+            server1.stop(function(){
+              server2.stop(done)
+            })
+          }
+        })
+      })
+
+      server1.start(function(){
+        server2.start();
+      });
     })
   })
 })
