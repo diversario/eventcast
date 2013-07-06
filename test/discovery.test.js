235aa2dd9d6804ea5eb77380afd8dc5e053b03d3
ilya.shaisultanov <ilya.shaisultanov@gmail.com>
2013-07-06 10:37:19 -0400

    Use Event and EventList in the main lib

--- test/discovery.test.js
+++ test/discovery.test.js
@@ -4,10 +4,89 @@ var Disco = require('../')
 
 
 describe('Default announcements', function () {
+  function getOpts(custom) {
+    var opts = {
+      port: custom && custom.port || Disco.getRandomPort()
+    }
+    
+    custom && Object.keys(custom).forEach(function (k) {
+      opts[k] = custom[k]
+    })
+    
+    return opts
+  }
+  
   describe('Two instances', function () {
+    it('emit messages', function (done) {
+      var opts = getOpts()
+        , server1 = new Disco(opts)
+        , server2 = new Disco(opts)
+        , messageCount = 0
+
+      assert.notEqual(server1.id, server2.id)
+
+      ;[server1, server2].forEach(function(server){
+        server.on('disco:announce', function(msg){
+          assert(/^hello$/.test(msg.payload))
+          if (++messageCount == 2) {
+            server1.stop(function(){
+              server2.stop(done)
+            })
+          }
+        })
+      })
+
+      server1.start(function(){
+        server2.start()
+      })
+    })
+
+    it('register each other', function (done) {
+      var opts = getOpts()
+        , server1 = new Disco(opts)
+        , server2 = new Disco(opts)
+        , messageCount = 0
+
+      assert.notEqual(server1.id, server2.id)
+
+      ;[server1, server2].forEach(function(server){
+        server.on('disco:announce', function(msg){
+          assert(/^hello$/.test(msg.payload))
+          if (++messageCount == 6) {
+            getNodeList()
+          }
+        })
+      })
+
+      function getNodeList() {
+        var list1 = server1.nodes.toArray()
+          , list2 = server2.nodes.toArray()
+        
+        list1.sort(function (a, b) {
+          return (a.id < b.id) ? -1 : 1
+        })
+        
+        list2.sort(function (a, b) {
+          return (a.id < b.id) ? -1 : 1
+        })
+
+        assert.deepEqual(list1, list2)
+        
+        server1.stop(function(){
+          server2.stop(done)
+        })
+      }
+      
+      server1.start(function(){
+        server2.start()
+      })
+    })
+
+
     it('can see each other, encrypted body', function (done) {
-      var server1 = new Disco({encrypt: {'key': 'qwqweqweqweq'}})
-        , server2 = new Disco({encrypt: {'key': 'qwqweqweqweq'}})
+      var opts = getOpts({encrypt: {'key': 'qwqweqweqweq'}})
+        , server1 = new Disco(opts)
+        , server2 = new Disco(opts)
         , messageCount = 0
       
       assert.notEqual(server1.id, server2.id)
@@ -32,7 +111,7 @@ describe('Default announcements', function () {
     })
     
     it('do not interfere with each other, plaintext body', function (done) {
-      var opts = {multicastLoopback: true, port: Disco.getRandomPort()}
+      var opts = getOpts()
         , server1 = Disco(opts)
         , server2 = Disco(opts)
         , messageCount = 0
